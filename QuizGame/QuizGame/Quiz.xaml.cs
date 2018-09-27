6cd8226d4626e979d234338e26ecc2e3f83e91c5
joohokim <ravenerkr@gmail.com>
2018-09-28 00:49:21 +0900

    하드코드된 퀴즈. 중간저장.

--- QuizGame/QuizGame/Quiz.xaml.cs
+++ QuizGame/QuizGame/Quiz.xaml.cs
@@ -20,21 +20,92 @@ namespace QuizGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Quiz> _quizList = new List<Quiz>();
+        private Quiz _currentQuiz;
+        private int _currentQuizIndex;
+
         public MainWindow()
         {
             InitializeComponent();
+            MakeQuizList();
+            ShowNextQuiz();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ShowNextQuiz()
         {
-            if (HelloButton.IsChecked == true)
+            if (_currentQuizIndex == _quizList.Count - 1)
             {
-                MessageBox.Show("Hello.");
+                MessageBox.Show("다 풀었습니다.");
             }
-            else if (GoodbyeButton.IsChecked == true)
+            else
             {
-                MessageBox.Show("Goodbye.");
+                _currentQuiz = _quizList[_currentQuizIndex];
+
+                QuizTitle.Text = _currentQuiz.Title;
+                RadioAnswer1.Content = _currentQuiz.Answers[0];
+                RadioAnswer2.Content = _currentQuiz.Answers[1];
+                RadioAnswer3.Content = _currentQuiz.Answers[2];
+                RadioAnswer4.Content = _currentQuiz.Answers[3];
+
+                _currentQuizIndex++;
             }
+        }
+
+        private void MakeQuizList()
+        {
+            _quizList.Add(new Quiz("1+1 =? ", 0, new List<string>() { "2", "3", "4", "5" }));
+            _quizList.Add(new Quiz("2+3 =? ", 1, new List<string>() { "4", "5", "6", "코와붕가!" }));
+        }
+
+        private void NextQuizButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowNextQuiz();
+        }
+
+        private void RadioAnswer1_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckQuiz(_currentQuiz, 1);
+        }
+
+        private void RadioAnswer2_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckQuiz(_currentQuiz, 2);
+        }
+
+        private void RadioAnswer3_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckQuiz(_currentQuiz, 3);
+        }
+
+        private void RadioAnswer4_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckQuiz(_currentQuiz, 4);
+        }
+
+        private void CheckQuiz(Quiz currentQuiz, int i)
+        {
+            if (currentQuiz.AnswerNumber == i)
+            {
+                MessageBox.Show("맞았습니다! ^_^");
+            }
+            else
+            {
+                MessageBox.Show("틀렸습니다. ㅠㅠ");
+            }
+        }
+    }
+
+    public class Quiz
+    {
+        public string Title { get; }
+        public int AnswerNumber { get; }
+        public List<string> Answers = new List<string>();
+
+        public Quiz(string title, int answerNumber, List<string> answers)
+        {
+            Title = title;
+            AnswerNumber = answerNumber;
+            Answers = answers;
         }
     }
 }
